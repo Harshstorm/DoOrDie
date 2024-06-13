f11d5c010c8d59d27bd7f3c6c0a5b40179035887
Harshit . <jangra.harshit7065@gmail.com>
2024-06-14 02:29:59 +0530

    changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ Connection()
 const server = http.createServer(app)
 const io = new Server(server, {
     cors:{
-        origin: 'http://localhost:3000'
+        origin: 'https://animated-marshmallow-ffacab.netlify.app'
     }
 })
 
