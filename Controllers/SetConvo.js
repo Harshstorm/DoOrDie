688b815905d6cda26e95bb14fb942510410e5162
Harshit . <jangra.harshit7065@gmail.com>
2024-06-15 02:09:10 +0530

    hhjjgsfs

--- Controllers/SetConvo.js
+++ Controllers/SetConvo.js
@@ -1,4 +1,5 @@
 import ConvoModel from "../Models/Conversation.js";
+import MessagesModel from "../Models/Messages.js";
 
 export const setConvo = async(req, res)=>{
     try{
@@ -6,7 +7,10 @@ export const setConvo = async(req, res)=>{
         const receiverId = req.body.receiverId
         let exists = await ConvoModel.findOne({members: {$all:[senderId, receiverId]}})
         if(exists){
-            res.json(exists)
+            await MessagesModel.find({conversationId: exists._id })
+            .then(messages=>{
+                           res.json({allmessages: messages, convoId: exists._id})
+            })
         }else{
             const newConvo = new ConvoModel({
                 members: [senderId, receiverId]
@@ -15,8 +19,11 @@ export const setConvo = async(req, res)=>{
             await newConvo.save(); 
             // Find the conversation again after saving it
            const exists = await ConvoModel.findOne({ members: { $all: [senderId, receiverId] } });
-            res.json(exists); 
-        }
+           await MessagesModel.find({conversationId: exists._id })
+            .then(messages=>{
+                           res.json({allmessages: messages, convoId: exists._id})
+            })
+    }
     }catch(error){
         console.log(error.message)
     }
